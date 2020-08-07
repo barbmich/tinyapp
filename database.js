67c03152e7c53df179f84c37d97d1f4ffcc464cc
Michele Barbiero <babishhh@gmail.com>
2020-08-07 01:11:16 +0000

    added comments to database.js

--- database.js
+++ database.js
@@ -1,3 +1,6 @@
+
+// initial users database. I'm aware that their passwords are not hashed,
+// they are there to help with test cases
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -11,6 +14,8 @@ const users = {
   }
 };
 
+// initial short urls database. As above, this is to help testing without
+// having to always create entries when launching the server.
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
